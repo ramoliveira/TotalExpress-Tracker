b42b4e5e95df699940e48036780880f0b0537b87
Ramon Dias de Oliveira de Almeida <ramon.almeida@apoioprodesp.sp.gov.br>
2026-05-28 17:25:14 -0300

    refactor URL and headers

--- src/constants.sh
+++ src/constants.sh
@@ -5,5 +5,6 @@ readonly COOKIE_JAR="$COOKIE_DIR/totalexpress.cookiejar"
 
 readonly BASE_URL="https://totalconecta.totalexpress.com.br"
 readonly TRACKING_URL="$BASE_URL/rastreamento"
-readonly API_URL="$BASE_URL/mfe-rastreio/api/basic"
+readonly CHECK_URL="$BASE_URL/mfe-rastreio/api/basic"
+readonly STATUS_URL="$BASE_URL/rastreamento/rastreamento/encomendas"
 
--- src/cookies.sh
+++ src/cookies.sh
@@ -10,12 +10,6 @@ curl_with_cookies() {
     curl \
       -c "$COOKIE_JAR" \
       -b "$COOKIE_JAR" \
-      "$@"
-}
-
-bootstrap_session() {
-    curl_with_cookies \
-      -s \
       -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
       -H "Accept-Encoding: gzip, deflate, br, zstd" \
       -H "Accept-Language: pt-BR,pt;q=0.9" \
@@ -28,6 +22,13 @@ bootstrap_session() {
       -H "Sec-Fetch-User: ?1" \
       -H "Upgrade-Insecure-Requests: 1" \
       -H "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Mobile Safari/537.36" \
+      -H "Referer: $TRACKING_URL" \
+      "$@"
+}
+
+bootstrap_session() {
+    curl_with_cookies \
+      -s \
       "$TRACKING_URL" \
       > /dev/null
 }
